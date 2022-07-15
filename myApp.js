9f20a86fc2f46fec7e016e16d1d46c0a0505a457
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-07-15 17:06:58 -0500

    CRUD Part I- Create

--- myApp.js
+++ myApp.js
@@ -4,7 +4,38 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const someFunc = function(done) {
+  //... do something (risky) ...
 
+  let personSchema = new mongoose.Schema({
+    name: {type: String},
+    age: {type: Number},
+    favoriteFoods: {type: Array}
+  })
+
+  module.exports = mongoose.model('Person', personSchema) 
+  
+  let PersonModel = require('./person')
+
+  let person = new PersonModel({
+    name: 'Adam',
+    age: 28,
+    favoriteFoods: 'ice cream'
+  })
+
+  person.save()
+    .then(doc => {
+      console.log(doc)
+    })
+
+    .catch(err => {
+      console.error(err)
+    })
+    if (error) return done(error);
+    done(null, result);
+  };
+  
+    
 let Person;
 
 const createAndSavePerson = (done) => {
