011c6a22815bb424f003a216f4caf927dcffc699
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-07-29 16:11:58 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -30,7 +30,7 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  Person.find(personName, (error, data) => {
+  Person.find({name: personName}, (error, data) => {
     if(error) return console.log(error);
     done(null, data);
   });
