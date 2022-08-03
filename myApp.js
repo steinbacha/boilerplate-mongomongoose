dae3652d9381ac538b420bafeee83ea2e6e0e5f2
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-08-03 17:16:25 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -92,8 +92,12 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person
+  .find({favoriteFoods: foodToSearch})
+  .sort({ name })
+  .limit(2)
+  .select('-age')
+  .exec(done(err, data));
 };
 
 /** **Well Done !!**
